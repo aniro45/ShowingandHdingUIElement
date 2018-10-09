77effa4b4a71acf5b2bb6dde092a439bfb4c7b6a
Admin <aniket.jadhav.8151@gmail.com>
2018-10-09 20:30:17 +0530

    Commit

--- app/src/main/java/com/example/admin/showingandhdinguielement/MainActivity.java
+++ app/src/main/java/com/example/admin/showingandhdinguielement/MainActivity.java
@@ -2,8 +2,23 @@ package com.example.admin.showingandhdinguielement;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    public void showClicked(View view){
+
+        textView = (TextView) findViewById(R.id.textViewId);
+
+        textView.setVisibility(View.VISIBLE);
+    }
+
+    public void hideClicked(View view){
+        textView.setVisibility(View.INVISIBLE);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
